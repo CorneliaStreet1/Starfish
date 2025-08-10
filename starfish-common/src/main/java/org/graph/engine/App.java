e4e24cec1593813c65eabb1fb56d6899f8307f7f
jiangyiqing <1181385834@qq.com>
2025-08-10 23:25:22 +0800

    工程初始化

---
+++ starfish-common/src/main/java/org/graph/engine/App.java
@@ -0,0 +1,13 @@
+package org.graph.engine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ starfish-core/src/main/java/org/graph/engine/App.java
@@ -0,0 +1,11 @@
+package org.graph.engine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
